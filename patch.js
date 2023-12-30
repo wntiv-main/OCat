35a9d44e91783a6007bb07578d7bad1188c412eb
wntiv-main <60457971+wntiv-main@users.noreply.github.com>
2023-12-30 13:33:07 +1300

    New hash

--- patch.js
+++ patch.js
@@ -1,7 +1,7 @@
 if(!!ocat) throw new Error("Already Injected");
 
 var ocat = {
-	_LAST_SEEN_CCAT_HASH: 1702798979,
+	_LAST_SEEN_CCAT_HASH: 1596547617,
 	_notification: new Audio(),
 	_currentNotification: null,
 	_currentBannerColor: "transparent",
